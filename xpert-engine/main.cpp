31a226f4ecb806b827dd18bba80e03299cb87bdb
DEV-PC\DEV <rlcioletti@gmail.com>
2018-01-28 00:19:42 -0200

    Init woking

--- xpert-engine/main.cpp
+++ xpert-engine/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, const char * argv[]) {
 	
 	game = new Game();
 
+	game->init("XpertEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
 	while (game->running()) {
 		game->handleEvents();
 		game->update();
